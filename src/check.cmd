8374adafb60c8f8f405d90489903adf5ef119c8c
Victor Derks <vbaderks@gmail.com>
2015-07-18 18:05:06 +0200

    Batch and config file to use cppcheck

---
+++ src/check.cmd
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Cppcheck\cppcheck.exe" --enable=all --platform=win32W -D_WIN32 -D_M_X64 -D_M_AMD64 -D__cplusplus --report-progress -I "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\include" -I "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\atlmfc\include" -I "C:\Program Files (x86)\Windows Kits\8.1\Include\shared" vvvsample
