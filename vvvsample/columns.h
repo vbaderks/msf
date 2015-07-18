206658b9dedc1bf443881cbf10d03c2ba8252ce6
Victor Derks <vbaderks@gmail.com>
2015-07-18 17:23:21 +0200

    restructure of the file layout: adding a "src" folder

--- vvvsample/columns.h
+++
@@ -1,9 +0,0 @@
-//
-// (C) Copyright by Victor Derks
-//
-// See README.TXT for detailed details of the software licence.
-//
-#pragma once
-
-const int COLUMN_NAME = 0;
-const int COLUMN_SIZE = 1;
--- vvvtest/stdafx.cpp
+++
@@ -1,6 +0,0 @@
-//
-// (C) Copyright by Victor Derks
-//
-// See README.TXT for the details of the software licence.
-//
-#include "stdafx.h"
